73aa9e4c12d75c371aad76cfbd73f9c0d52bed9c
TERRAS Juba <ij_terras@esi.dz>
2022-05-16 15:09:00 +0100

    Code sequentiel

---
+++ cmake-build-debug/CMakeFiles/Projet_HPC.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projet_HPC.dir/main.c.obj"
+  "Projet_HPC.exe"
+  "Projet_HPC.exe.manifest"
+  "Projet_HPC.pdb"
+  "libProjet_HPC.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projet_HPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
